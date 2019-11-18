a8baed2167dcaac45f2b3d6d8d7793bfc729e655
ClementGao <809668491@qq.com>
2019-11-19 01:11:15 +0800

    s

--- start.sh
+++ start.sh
@@ -3,5 +3,8 @@ dotnet build
 dotnet publish -c:Release -o /home/projects/mysqlweb/publish
 cd publish
 docker login -u clementgao -p clement@@8767
+docker rmi $(docker images -f “dangling=true” -q)
+docker rmi `docker images | grep "<none>" | awk '{print $3}'`
+docker rmi -f clementgao/mysqlweb
 docker build -t clementgao/mysqlweb .
 docker push clementgao/mysqlweb:latest
