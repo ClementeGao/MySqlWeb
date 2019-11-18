3d146d28794d8edb493fcb05afb1968731610426
ClementGao <809668491@qq.com>
2019-11-19 00:55:51 +0800

    n

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ dotnet build
 dotnet publish -c:Release -o /home/projects/mysqlweb/publish
 cd publish
 docker login -u clementgao -p clement@@8767
-docker build -t mysqlweb .
+docker build -t clementgao/mysqlweb .
 docker push clementgao/mysqlweb:latest
