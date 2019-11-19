0654e3f2e235ecd3a0ef1b57fb12c9aa4212eb41
ClementGao <809668491@qq.com>
2019-11-19 23:56:00 +0800

    none

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ docker rmi $(docker images -f “dangling=true” -q)
 docker rmi `docker images | grep "<none>" | awk '{print $3}'`
 docker rmi -f clementgao/mysqlweb
 docker build -t clementgao/mysqlweb .
-docker push clementgao/mysqlweb:latest
+#docker push clementgao/mysqlweb:latest
